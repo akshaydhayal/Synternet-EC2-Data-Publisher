84ce5f0750666ac652695942a67ed3ffc254e9e1
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-10-03 03:51:39 +0530

    Puvlishing both live matches and its stats

--- examples/publish.ts
+++ examples/publish.ts
@@ -10,34 +10,88 @@ const subject = "stark.sports.data";
 const publisherAccessToken = process.env.PUBLISH_ACCESS_TOKEN??"";
 
 
+
+async function publishServiceFn(publishService) {
+  try {
+    if (publishService) {
+      const subject = "stark.sports.data";
+      let count = 1;
+
+      const fetchAndPublish = async () => {
+        try {
+          let liveMatchesResponse = await fetch("https://cricbuzz-live.vercel.app/v1/matches/live", {
+            method: "GET",
+          });
+          let liveMatchesData = await liveMatchesResponse.json();
+          let liveMatchesId = liveMatchesData.data.matches.map((match) => match.id);
+          console.log(liveMatchesId);
+
+          let toPublishData = {
+            liveMatchDetails: liveMatchesData.data.matches,
+            liveMatchStats: [],
+          };
+          const fetchAndPublishScores = async () => {
+            try {
+              // let toPublishData = [];
+              toPublishData.liveMatchStats = [];
+              const responses = await Promise.all(liveMatchesId.map((id) => fetch(`https://cricbuzz-live.vercel.app/v1/score/${id}`)));
+              for (let i = 0; i < responses.length; i++) {
+                const matchData = await responses[i].json();
+                // toPublishData.push({ [liveMatchesId[i]]: { ...matchData.data, rand: Math.random() } });
+                // toPublishData.liveMatchStats.push({ [liveMatchesId[i]]: matchData.data });
+                toPublishData.liveMatchStats.push({ [liveMatchesId[i]]: { ...matchData.data, update: matchData.data.update + Math.random() } });
+              }
+              console.log(`Publishing message count : ${count} ${JSON.stringify(toPublishData)}`);
+              // console.log(`Publishing message count : ${count} `);
+              await publishService?.publishJSON(subject, toPublishData);
+              count++;
+            } catch (error) {
+              console.error("Failed to fetch match scores:", error);
+            }
+          };
+
+          await fetchAndPublishScores();
+          setInterval(fetchAndPublishScores, 10000); // Every minute
+        } catch (e) {
+          console.error("Failed to fetch live matches:", e);
+        }
+      };
+
+      await fetchAndPublish();
+      setInterval(fetchAndPublish, 3600000); // Every hour
+    }
+  } catch (e) {
+    console.error("Error publishing to NATS:", e);
+  }
+}
+
 async function main() {
   // Connect to the NATS server with credentials
   const service = new NatsService({
     url: natsUrl,
     natsCredsFile: createAppJwt(publisherAccessToken),
-    // natsCredsFile: publisherNatsCredsFile,
   });
 
   console.log("Connecting to NATS server...");
   await service.waitForConnection();
   console.log("Connected to NATS server.");
 
-  // const message = { hello: "world!" };
-  let count = 1;
-  setInterval(async () => {
-    try {
-      const response = await fetch("https://cricbuzz-live.vercel.app/v1/score/100229", {
-        method: "GET",
-      });
-      const data = await response.json();
-      // const message = { hello: "world! "+count };
-      console.log(`Publishing message : ${count} ${JSON.stringify(data.data)} to subject: ${subject}`);
-      await service.publishJSON(subject, data.data);
-      count++;
-    } catch (error) {
-      console.error("Failed to fetch data after multiple attempts:", error);
-    }
-  }, 10000);
+  publishServiceFn(service);
+
+  // let count = 1;
+  // setInterval(async () => {
+  //   try {
+  //     const response = await fetch("https://cricbuzz-live.vercel.app/v1/score/100229", {
+  //       method: "GET",
+  //     });
+  //     const data = await response.json();
+  //     console.log(`Publishing message : ${count} ${JSON.stringify(data.data)} to subject: ${subject}`);
+  //     await service.publishJSON(subject, data.data);
+  //     count++;
+  //   } catch (error) {
+  //     console.error("Failed to fetch data after multiple attempts:", error);
+  //   }
+  // }, 10000);
 }
 
 main().catch((err) => {
